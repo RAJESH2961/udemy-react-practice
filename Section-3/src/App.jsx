80d62fabbc4e86efc4e6024bb16b822ee1b9263f
Rajesh <grajesh2906@gmail.com>
2025-04-25 16:10:14 +0530

    Added Good Practice code for section-42 for dynamic values

--- Section-3/src/App.jsx
+++ Section-3/src/App.jsx
@@ -1,15 +1,44 @@
+// //PS create i want to display some words randomly in h1 tag instead of Fundamantals
+// const reactDescription = ['Fundamantal', 'Crucial', 'Core']
+
+// function genRandomInt(max){
+//   return Math.floor(Math.random() * (max+1));
+// }
+// function Header(){
+//   return(
+//     <header>
+//         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+//         <h1>React Essentials</h1>
+//                <p>
+//           {reactDescription[genRandomInt(2)]} React concepts you will need for almost any app you are
+//           going to build!
+//                </p>
+//       </header>
+//   )
+// }
+
+
+//Good Practice
+const reactDescription = ['Fundamantal', 'Crucial', 'Core']
+
+function genRandomInt(max){
+  return Math.floor(Math.random() * (max+1));
+}
 function Header(){
+  //Good Practice
+  const description = reactDescription[genRandomInt(2)];
   return(
     <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
                <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
                </p>
       </header>
   )
 }
+
 
 function App() {
   return (
