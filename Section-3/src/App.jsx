9da1177aa0575730137bd7bf70da1e11ffab6ced
Rajesh <grajesh2906@gmail.com>
2025-04-26 15:19:07 +0530

    Another methof for conditional Rendering

--- Section-3/src/App.jsx
+++ Section-3/src/App.jsx
@@ -148,7 +148,8 @@ function App() {
 
           {/* <TabButton label="Hello"></TabButton> */}
           </menu>
-          {!selectedTopic ?(<p>Please select a topic </p> ): (<div id="tab-content">
+          {!selectedTopic && (<p>Please select a topic </p> )}
+          {selectedTopic && (<div id="tab-content">
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
             <pre>
