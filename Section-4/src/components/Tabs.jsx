0837009fc41608a2160634a47a0ee60ddf659797
Rajesh <grajesh2906@gmail.com>
2025-04-27 21:14:53 +0530

    Added Components Types Dynamically

--- Section-4/src/components/Tabs.jsx
+++ Section-4/src/components/Tabs.jsx
@@ -1,6 +1,7 @@
 export default function Tabs({ children, buttons, ButtonsContainer }) {
     return (
       <>
+      //Here the ButtonContainer name is menu passed from props if you want to verify indpect the tools
         <ButtonsContainer>{buttons}</ButtonsContainer>
         {children}
       </>
