e876d352385d4e860bc3ac571c0ced33b2ef0f8e
Rajesh <grajesh2906@gmail.com>
2025-05-03 20:55:46 +0530

    Added Tic tac toe game board

--- Projects/Tic-Tac-Toe/src/App.jsx
+++ Projects/Tic-Tac-Toe/src/App.jsx
@@ -1,14 +1,17 @@
+// Add this at the top of App.jsx or index.js
+import './index.css';
 import Player from "./Components/Player";
 // index.js or App.js
-import './index.css';  // or './App.css'
+import GameBoard from "./Components/GameBoard";
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="player 1" symbol="X" />
-          <Player name="player 2" symbol="O" />
+          <Player initialName="player 1" symbol="X" />
+          <Player initialName="player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
--- Projects/Tic-Tac-Toe/src/Components/Player.jsx
+++ Projects/Tic-Tac-Toe/src/Components/Player.jsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
-export default function Player({name,symbol}){
+export default function Player({initialName,symbol}){
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName)
 
     function handleEditClick(){
         // setIsEditing(isEditing ? false : true); its an complex
         // setIsEditing(!isEditing);//it will invert but its not a good practice
         setIsEditing((editing) => !editing);
     }
-    let playerName =<span className="player-name">{name}</span>;
+    function handleChange(event){
+        console.log(event);
+        setPlayerName(event.target.value);   
+    }
+
+    let editableplayerName =<span className="player-name">{playerName}</span>;
     // let btnCaption = "Edit";
     if(isEditing){
-        playerName = <input type="text" required value={name} />
+        editableplayerName = <input type="text" required value={playerName} onChange={handleChange}/>
         // let btnCaption = "Save";
 
     }
     return(
         <li>
             <span>
-            {playerName}
+            {editableplayerName}
             <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
