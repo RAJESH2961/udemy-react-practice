2225f04911caa74381bfc02057af4067040f6a96
Rajesh <grajesh2906@gmail.com>
2025-05-04 19:17:01 +0530

    Added Investment calculator on 4-05-2025

--- Projects/Tic-Tac-Toe/src/App.jsx
+++ Projects/Tic-Tac-Toe/src/App.jsx
@@ -3,12 +3,22 @@ import { useState } from 'react';
 import './index.css';
 import Player from "./Components/Player";
 // index.js or App.js
+import Log from './Components/Log';
 import GameBoard from "./Components/GameBoard";
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const[activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex){
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+      if(prevTurns.length>0 && prevTurns[0].player === 'X'){
+        currentPlayer = 'O';
+      }
+      const updatedTurns = [{square:{row : rowIndex, col: colIndex}, player:activePlayer},...prevTurns];
+      return updatedTurns;
+    });
   }
   return (
     <main>
@@ -19,6 +29,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
+    <Log/>
     </main>
   );
 }
