37f67b195ca8d2ce5affea5fcc72260c1453d69d
Rajesh <grajesh2906@gmail.com>
2025-05-05 18:27:23 +0530

    Added reusable Components & Components Combinations

--- Projects/Form-validation/src/components/AuthInputs.jsx
+++ Projects/Form-validation/src/components/AuthInputs.jsx
@@ -7,41 +7,23 @@ const ControlContainer = styled.div`
   gap: 0.5rem;
   margin-bottom: 1.5rem;
 `
-const Label = styled.label`
- display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color:${({$invalid})=> $invalid ? '#f87171' : '#6b7280'};
-`
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => $invalid ? '#fed3d2' : '#d1d5db' };
-  color: ${({ $invalid }) => $invalid ? '#ef4444' : '#374151' };
-  border: 1px solid ${({ $invalid }) => $invalid ? '#f73f3f' : 'transparent' };
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1),
-              0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
 
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
+import Button from './Button.jsx';
+import Input from './Input.jsx';
+// const Button = styled.button`
+//   padding: 1rem 2rem;
+//   font-weight: 600;
+//   text-transform: uppercase;
+//   border-radius: 0.25rem;
+//   color: #1f2937;
+//   background-color: #f0b322;
+//   border-radius: 6px;
+//   border: none;
 
-  &:hover{
-    background-color: #f0920e;
-}
-`
+//   &:hover{
+//     background-color: #f0920e;
+// }
+// `
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -66,11 +48,11 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid} >Email</Label>
+
           <Input
+          label="Email"
             type="email"
-            $invalid={emailNotValid}
+            invalid={emailNotValid}
             // style={{
             //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
             // }}
@@ -78,24 +60,22 @@ export default function AuthInputs() {
             // className={emailNotValid && 'invalid'} //if it is false it returns false so it is not a good practice but it works gives erroe in browser
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        </p>
-        <p>
-          <Label $invalid = {passwordNotValid}>Password</Label>
+
           <Input
+          label="password"
             type="password"
-            $invalid={passwordNotValid}
+            invalid={passwordNotValid}
             // className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <Button  onClick={handleLogin}>Sign In</Button>
+        <Button className='button' onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
