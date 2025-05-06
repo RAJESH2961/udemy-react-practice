4f3cd501ac1e871a7fba0d3400b6231b2e26cfe5
Rajesh <grajesh2906@gmail.com>
2025-05-06 10:43:29 +0530

    Added Refs code for Connecting & Accessing HTML elements

--- Section-8/src/components/Player.jsx
+++ Section-8/src/components/Player.jsx
@@ -1,10 +1,26 @@
+import { useState, useRef } from "react";
 export default function Player() {
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  // const [submitted, setSubmitted] = useState(false);
+  //function to handle Name
+  // function handleName(event){
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(event.target.value);
+  // }
+  function handleClick(){
+    // setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/* <h2>Welcome {enteredPlayerName ? enteredPlayerName : 'Unknown User'}</h2> */}
+      {/* or */}
+      <h2>Welcome {enteredPlayerName ??   'Unknown User'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input  type="text" ref={playerName} />
+        {/* <input onChange={handleName} type="text" value={enteredPlayerName} /> */}
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
