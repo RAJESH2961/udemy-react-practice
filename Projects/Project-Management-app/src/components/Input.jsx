812ccaadfafe2b2899df6013643893829df5b297
Rajesh <grajesh2906@gmail.com>
2025-05-07 11:11:28 +0530

    added stylings to input fields and created shared Tailwind classes

--- Projects/Project-Management-app/src/components/Input.jsx
+++ Projects/Project-Management-app/src/components/Input.jsx
@@ -1,6 +1,18 @@
-export default function Input({label, textArea, ...props}){
-    return <p>
-        <label htmlFor="">{label}</label>
-        {textArea ? <textArea {...props}/> : <input {...props} />}
-    </p>
-}
+export default function Input({ label, textArea, ...props }) {
+    const sharedClasses =
+      "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+  
+    return (
+      <p className="flex flex-col gap-1 my-4">
+        <label className="text-sm font-bold uppercase text-stone-500" htmlFor={props.id || ""}>
+          {label}
+        </label>
+        {textArea ? (
+          <textarea className={sharedClasses} {...props} />
+        ) : (
+          <input className={sharedClasses} {...props} />
+        )}
+      </p>
+    );
+  }
+  
