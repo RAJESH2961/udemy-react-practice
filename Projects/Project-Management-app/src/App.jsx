21764ec6d41856cc2cac2f6a72b363382b70a0d1
Rajesh <grajesh2906@gmail.com>
2025-05-07 11:29:48 +0530

    Added No Project seleected componet and seperated a button component and resued fro various places

--- Projects/Project-Management-app/src/App.jsx
+++ Projects/Project-Management-app/src/App.jsx
@@ -1,12 +1,13 @@
 import ProjectSidebar from "./components/ProjectSidebar";
 import NewProject from "./components/NewProject";
+import NoProjectselected from "./components/NoProjectSelected";
 function App() {
   return (
     // <>
       // {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <main className="h-screen my-8 mx-20 flex gap-8">
         <ProjectSidebar/>
-        <NewProject />
+        <NoProjectselected />
       </main>
     // </>
   );
