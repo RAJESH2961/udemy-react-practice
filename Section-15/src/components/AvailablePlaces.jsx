ebc398cdf823ba930f470721c5452dedc8828fc6
Rajesh <grajesh2906@gmail.com>
2025-05-13 15:32:04 +0530

    added data fetching code

--- Section-15/src/components/AvailablePlaces.jsx
+++ Section-15/src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
 import Places from './Places.jsx';
+import { useState, useEffect } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+// using this useEffect we can ensure that this code will executed only once after the component executes
+  useEffect(() => {
+    fetch('http://localhost:3000/places') // Step 1: Send a GET request to your backend
+      .then((response) => response.json()) // Step 2: Convert the HTTP response to JSON
+      .then((resData) => {
+        setAvailablePlaces(resData.places); // Step 3: Update state with the "places" data
+      })
+      .catch((error) => {
+        console.error('Error fetching places:', error); // Step 4: Handle any errors
+      });
+  }, []); // 👈 Empty dependency array = run only once when component mounts
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
