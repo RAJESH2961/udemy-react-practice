bc55190ffbd7007b885debe1ee808d7e5f9bb196
Rajesh <grajesh2906@gmail.com>
2025-05-14 09:06:11 +0530

    fetching data using mehtod-2 (Async / await )

--- Section-15/src/components/AvailablePlaces.jsx
+++ Section-15/src/components/AvailablePlaces.jsx
@@ -4,15 +4,22 @@ import { useState, useEffect } from 'react';
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 // using this useEffect we can ensure that this code will executed only once after the component executes
+
+//Another method to fetch data using async and await keyword
   useEffect(() => {
-    fetch('http://localhost:3000/places') // Step 1: Send a GET request to your backend
-      .then((response) => response.json()) // Step 2: Convert the HTTP response to JSON
-      .then((resData) => {
-        setAvailablePlaces(resData.places); // Step 3: Update state with the "places" data
-      })
-      .catch((error) => {
-        console.error('Error fetching places:', error); // Step 4: Handle any errors
-      });
+    async function fetchPlaces(){
+      const response = await fetch('http://localhost:3000/places') // Step 1: Send a GET request to your backend
+      const resData = await response.json(); // Step 2: Convert the HTTP response to JSON
+      setAvailablePlaces(resData.places); // Step 3: Update state with the "places" data
+
+    }
+    fetchPlaces();
+      // .then((response) => response.json()) 
+      // .then((resData) => {
+      // })
+      // .catch((error) => {
+      //   console.error('Error fetching places:', error); // Step 4: Handle any errors
+      // });
   }, []); // 👈 Empty dependency array = run only once when component mounts
 
   return (
