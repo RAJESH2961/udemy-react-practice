974ef7a5c010e3b2505a66b30e6dcf363fba2576
Rajesh <grajesh2906@gmail.com>
2025-05-14 17:01:56 +0530

    sending selected places to backend

--- Section-15/src/http.js
+++ Section-15/src/http.js
@@ -13,3 +13,19 @@ export async function fetchAvailablePlaces() {
     throw error;
   }
 }
+
+//method to send data to backend
+export async function updateUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({places}),//Passing the data by converting into json
+    headers: {
+      'content-Type': 'application/json'
+    }
+  });
+  const resData = await response.json();
+  if (!response.ok) {
+      throw new Error('Failed to Update useer data ');
+    }
+    return resData.message;
+}
