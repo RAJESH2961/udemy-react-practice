50b3eeabf318e99626634005fe0dfb2d6bb8e0fa
Rajesh <grajesh2906@gmail.com>
2025-05-14 11:40:02 +0530

    Added Error handleing UI when an error occured

--- Section-15/src/components/AvailablePlaces.jsx
+++ Section-15/src/components/AvailablePlaces.jsx
@@ -1,32 +1,38 @@
 import Places from './Places.jsx';
 import { useState, useEffect } from 'react';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   //maintaing state for loading and non loading while data
   const [isloading, setIsLoading] = useState();
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isError, setIsError] = useState();
 // using this useEffect we can ensure that this code will executed only once after the component executes
 
 //Another method to fetch data using async and await keyword
-  useEffect(() => {
-    async function fetchPlaces(){
-      setIsLoading(true);
-      const response = await fetch('http://localhost:3000/places') // Step 1: Send a GET request to your backend
-      console.log("Result Response",response);
-      const resData = await response.json(); // Step 2: Convert the HTTP response to JSON
-      console.log("Result Data",resData);
-      
-      setAvailablePlaces(resData.places); // Step 3: Update state with the "places" data
+ useEffect(() => {
+  async function fetchPlaces() {
+    setIsLoading(true);
+    try {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      if (!response.ok) {
+        throw new Error('Failed to fetch places');
+      }
+      setAvailablePlaces(resData.places);
+    } catch (error) {
+      setIsError({message : error.message || "An error occured while fetching data"});//Fallback text if the error message is not produced then it will take
+    } finally {
       setIsLoading(false);
     }
-    fetchPlaces();
-      // .then((response) => response.json()) 
-      // .then((resData) => {
-      // })
-      // .catch((error) => {
-      //   console.error('Error fetching places:', error); // Step 4: Handle any errors
-      // });
-  }, []); // 👈 Empty dependency array = run only once when component mounts
+  }
+
+  fetchPlaces();
+}, []);
+
+  if (isError) {
+    return <Error title="An Error Occurred" message={isError.message} />;
+  }
 
   return (
     <Places
