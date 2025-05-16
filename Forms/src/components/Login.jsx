1e9331d994e2a956d8ce272b972bee9a4f189584
Rajesh <grajesh2906@gmail.com>
2025-05-16 11:07:05 +0530

    form handling using seperate use state

--- Forms/src/components/Login.jsx
+++ Forms/src/components/Login.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 export default function Login() {
+  const [enterdEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+      // console.log(enterdEmail);
 
   function handleSubmit(event){
     event.preventDefault();
     console.log('submitted');
+  }
+
+  function handleEmailChange(event){
+    console.log(enterdEmail);
+    setEnteredEmail(event.target.value);
+    
+    
+  }
+    function handlePasswordChange(event){
+    setEnteredPassword(event.target.value);
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -11,12 +25,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+           id="email" 
+           type="email" 
+           name="email" 
+           onChange={handleEmailChange}
+           value={enterdEmail} 
+           />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input 
+          id="password"
+          type="password" 
+          name="password" 
+          onChange={handlePasswordChange}
+          value={enteredPassword}
+          />
         </div>
       </div>
 
