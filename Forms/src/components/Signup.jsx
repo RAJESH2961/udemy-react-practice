5dbb8411e4db9135760077dc994ca98db4ba8d3f
Rajesh <grajesh2906@gmail.com>
2025-05-16 13:05:02 +0530

    getting form data using FormData()

---
+++ Forms/src/components/Signup.jsx
@@ -0,0 +1,125 @@
+export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(event.target);
+    
+
+    const fd = new FormData(event.target);
+
+    const enteredEmail = fd.get("email");
+    const enteredPassword = fd.get("password");
+    const confirmPassword = fd.get("confirm-password");
+    const firstName = fd.get("first-name");
+    const lastName = fd.get("last-name");
+    const role = fd.get("role");
+    const acquisitions = fd.getAll("acquisition"); // multiple checkboxes
+    const termsAccepted = fd.get("terms");
+
+    // console.log("Email:", enteredEmail);
+    // console.log("Password:", enteredPassword);
+    // console.log("Confirm Password:", confirmPassword);
+    // console.log("First Name:", firstName);
+    // console.log("Last Name:", lastName);
+    // console.log("Role:", role);
+    // console.log("Acquisition Sources:", acquisitions);
+    // console.log("Accepted Terms:", termsAccepted === "on");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
+
+      <div className="control">
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" name="email" />
+      </div>
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" />
+        </div>
+
+        <div className="control">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            id="confirm-password"
+            type="password"
+            name="confirm-password"
+          />
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" name="first-name" />
+        </div>
+
+        <div className="control">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="last-name" />
+        </div>
+      </div>
+
+      <div className="control">
+        <label htmlFor="role">What best describes your role?</label>
+        <select id="role" name="role">
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+          <option value="employee">Employee</option>
+          <option value="founder">Founder</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className="control">
+          <input
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+          />
+          <label htmlFor="google">Google</label>
+        </div>
+
+        <div className="control">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+          />
+          <label htmlFor="friend">Referred by friend</label>
+        </div>
+
+        <div className="control">
+          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <label htmlFor="other">Other</label>
+        </div>
+      </fieldset>
+
+      <div className="control">
+        <label htmlFor="terms-and-conditions">
+          <input type="checkbox" id="terms-and-conditions" name="terms" />
+          I agree to the terms and conditions
+        </label>
+      </div>
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Sign up
+        </button>
+      </p>
+    </form>
+  );
+}
