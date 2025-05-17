8356ef075d4e0b4b3d4286d1cf2092c1389bea46
Rajesh <grajesh2906@gmail.com>
2025-05-17 14:25:15 +0530

    Added form handling

--- Projects/Opinion-Board/src/components/NewOpinion.jsx
+++ Projects/Opinion-Board/src/components/NewOpinion.jsx
@@ -1,8 +1,39 @@
+import { useActionState } from "react";
+
 export function NewOpinion() {
+  function shareOpinionAction(formData){
+    const title = formData.get('title');
+    const body = formData.get('body');
+    const userName = formData.get('userName');
+
+    let errors = [];
+    if(title.trim().length < 5) {
+      errors.push('Title must be at least five characters long');
+    }
+
+    if(body.trim().length < 10 || body.trim().length > 300) {
+      errors.push('Opinion must be between 10 and 300 characters long');
+    }
+
+    if(!userName.trim()) {
+      errors.push('Please Provide your name');
+    }
+
+    if(errors.length > 0) {
+      return {errors, enteredValues: {
+        title,
+        body,
+        userName
+      },};
+    }
+    // submit to backend
+    return {errors: null};
+  }
+  // useActionState();
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={shareOpinionAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
