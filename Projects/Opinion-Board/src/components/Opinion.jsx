292047087daa6ab350a5a891825f92ea15267a51
Rajesh <grajesh2906@gmail.com>
2025-05-17 16:53:54 +0530

    Diabling button when user upvoted || downvoted for a while using the pending state from use Action State

--- Projects/Opinion-Board/src/components/Opinion.jsx
+++ Projects/Opinion-Board/src/components/Opinion.jsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use,useActionState } from "react";
 import { OpinionsContext } from "../store/opinions-context";
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
@@ -10,6 +10,8 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
     // console.log('DOWNVOTE'); 
     await downvoteOpinion(id);
   }
+  const [upvoteFormState, upvoteFormAction, upvotePending] = useActionState(upvoteAction);
+  const [downvoteFormState, downvoteFormAction, downvotePending] = useActionState(downvoteAction);
   return (
     <article>
       <header>
@@ -18,7 +20,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button formAction={upvoteAction}>
+        <button formAction={upvoteFormAction} disabled={upvotePending || downvotePending}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -38,7 +40,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <span>{votes}</span>
 
-        <button formAction={downvoteAction}>
+        <button formAction={downvoteFormAction} disabled={upvotePending || downvotePending}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
