847c130a51eddb86d2481194bd7d0dc5a3fd7162
Rajesh <grajesh2906@gmail.com>
2025-05-17 14:32:35 +0530

    Handlign form submission using useActionState

--- Projects/Opinion-Board/src/components/NewOpinion.jsx
+++ Projects/Opinion-Board/src/components/NewOpinion.jsx
@@ -1,7 +1,7 @@
 import { useActionState } from "react";
 
 export function NewOpinion() {
-  function shareOpinionAction(formData){
+  function shareOpinionAction(prevState,formData){
     const title = formData.get('title');
     const body = formData.get('body');
     const userName = formData.get('userName');
@@ -29,26 +29,29 @@ export function NewOpinion() {
     // submit to backend
     return {errors: null};
   }
-  // useActionState();
+  const [formState, formAction] = useActionState(shareOpinionAction, {errors: null});
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form action={shareOpinionAction}>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
+            <input type="text" id="userName" name="userName" defaultValue={formState.enteredValues?.userName} />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
+            <input type="text" id="title" name="title" defaultValue={formState.enteredValues?.title} />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
+          <textarea id="body" name="body" rows={5} defaultValue={formState.enteredValues?.body}></textarea>
         </p>
+        {formState.errors && <ul className="errors">
+          {formState.errors.map((error) => (<li key={error}>{error}</li>))}
+          </ul>}
 
         <p className="actions">
           <button type="submit">Submit</button>
