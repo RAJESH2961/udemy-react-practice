3a773f1616fd199c2678d3a47d520ce6f1b01fa4
Rajesh <grajesh2906@gmail.com>
2025-05-18 18:49:07 +0530

    Designed the cartItems component seperately

--- Projects/Food-Ordering-app/src/components/Cart.jsx
+++ Projects/Food-Ordering-app/src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 
 export default function Cart(){
@@ -21,7 +22,7 @@ export default function Cart(){
     return <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
         <h2>Your cart</h2>
         <ul>
-            {cartCtx.items.map(item => <li key={item.id}>{item.name} - {item.quantity}</li>)}
+            {cartCtx.items.map(item => (<CartItem key={item.id} name={item.name} quantity={item.quantity} price={item.price} onIncrease={() => cartCtx.addItem(item)} onDecrease={() => cartCtx.removeItem(item.id)} />))}
         </ul>
         <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
         <p className="modal-actions">
---
+++ Projects/Food-Ordering-app/src/components/CartItem.jsx
@@ -0,0 +1,16 @@
+import { currencyFormatter } from "../util/formatting"
+export default function CartItem({name, quantity, price, onIncrease, onDecrease}) {
+    return <li className="cart-item">
+        <p>
+            {name} - {quantity} X {currencyFormatter.format(price)}
+        </p>
+        <p className="cart-item-actions">
+            {/* //OnIncrease and OnDecrease function is received as an props from parent component which is called the CartContext at the end */}
+            {/* In the end both will call the add item to cart and remove item from cart function which is located in CartContect */}
+            {/* the parent component have the acess to context */}
+            <button onClick={onDecrease}>-</button>
+            <span>Qty</span>
+            <button onClick={onIncrease}>+</button>
+        </p>
+    </li>
+}
