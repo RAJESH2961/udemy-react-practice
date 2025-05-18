53d035756f301e3c2631ff948d012767d8fb5c38
Rajesh <grajesh2906@gmail.com>
2025-05-18 16:19:25 +0530

    Added cart Removal code

--- Projects/Food-Ordering-app/src/store/CartContext.jsx
+++ Projects/Food-Ordering-app/src/store/CartContext.jsx
@@ -36,14 +36,47 @@ function cartReducer(state,action){
     }
 
     if(action.type === 'REMOVE_ITEM') {
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);//It returns true if it found item else -1
+        // 
+        const existingCartItem = state.items[existingCartItemIndex];
 
+        const updatedItems = [...state.items];
+
+        //it is is 1 then there is only one element we need to remove entire element in the cart
+        // THere are different ways to remove
+        if(existingCartItem.quantity === 1){
+            // splice takes index and no of items that should be spliced(remove) there 1 is remove one element
+            updatedItems.splice(existingCartItemIndex, 1);
+        }
+        // else update the quantity
+        else{
+            const updatedItem = {
+            ...existingCartItem,
+            quantity : existingCartItem.quantity -1,
+
+        }
+        updatedItems[existingCartItemIndex] = updatedItem;
     }
-    return state;
+        return {...state, items: updatedItems};
+}
+return state;
 }
 
 export function CartContextProvider({ children }) {
-    useReducer(cartReducer, {items:[]});
-    return <cartContext>{children}</cartContext>
+    const [cart, dispatchCartAction] = useReducer(cartReducer, {items:[]});
+
+    function addItem(item){
+        dispatchCartAction({type : 'ADD_ITEM', item: item});
+    }
+    function removeItem(id){
+        dispatchCartAction({type : 'REMOVE_ITEM', id: id});
+    }
+    const cartContext = {
+        items: cart.items,
+        addItem,
+        removeItem
+    }
+    return <cartContext value={cartContext}>{children}</cartContext>
 }
 
 export default cartContext;
