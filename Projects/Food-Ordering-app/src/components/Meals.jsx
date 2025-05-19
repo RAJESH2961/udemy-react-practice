5bbbbdf22100f98dccf4ccd2ef952e0ea73f8557
Rajesh <grajesh2906@gmail.com>
2025-05-19 09:39:42 +0530

    added a Cutom HTTP Hook to send request

--- Projects/Food-Ordering-app/src/components/Meals.jsx
+++ Projects/Food-Ordering-app/src/components/Meals.jsx
@@ -1,22 +1,19 @@
-import { useState, useEffect } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../Hooks/useHttp";
+
+const requestConfig = {  };
 export default function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
-    useEffect(() => {
-        async function fetchMeals(){
-        const response = await fetch('http://localhost:3000/meals');
-
-        if(!response.ok){
-            //do something
-        }
-        const meals = await response.json();
-        setLoadedMeals(meals);
-        }
-
-    fetchMeals();
-    },[]);//this empty dependencies will ensurer it is executed only once
-
+    const {data: loadedMeals, isLoading, error} = useHttp('http://localhost:3000/meals', requestConfig, [])
+    console.log(loadedMeals);
     
+    
+    if(isLoading){
+        return <p>Fetching meals...</p>
+    }
+
+    // if(!loadedMeals){
+    //     return <p>No meals found</p>
+    // }
 
     return <ul id="meals">
         {loadedMeals.map((meal) => (<MealItem key={meal.id} meal={meal} />))}
---
+++ Projects/Food-Ordering-app/src/hooks/useHttp.js
@@ -0,0 +1,42 @@
+import { useEffect, useState, useCallback } from "react";
+
+async function sendHttpRequest(url, config) {
+    const response = await fetch(url, config);
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error(resData.message || "Something went wrong, failed to send request");
+    }
+    return resData;
+}
+
+export default function useHttp(url, config = {}, initialData = []) {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState(initialData);
+
+    const sendRequest = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const resData = await sendHttpRequest(url, config);
+            setData(resData);
+        } catch (error) {
+            setError(error.message || "Something went wrong!");
+        }
+        setIsLoading(false);
+    }, [url, config]);
+
+    useEffect(() => {
+        if (!config.method || config.method.toUpperCase() === "GET") {
+            sendRequest();
+        }
+    }, [sendRequest]);
+
+    return {
+        data,
+        isLoading,
+        error,
+        sendRequest
+    };
+}
