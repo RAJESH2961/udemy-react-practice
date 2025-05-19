c9b9814275c73e67a27faf1f6b7f8a44a1a45f09
Rajesh <grajesh2906@gmail.com>
2025-05-19 22:05:39 +0530

    Dispatching Actions from inside components

--- Redux-Counter/src/components/Counter.js
+++ Redux-Counter/src/components/Counter.js
@@ -1,10 +1,18 @@
 import classes from './Counter.module.css';
-import { useSelector } from 'react-redux';// useSelector is a hook developed by react-redux
+import { useSelector, useDispatch } from 'react-redux';// useSelector is a hook developed by react-redux
 
 const Counter = () => {
+  const dispatch = useDispatch();// It will return an function we can pass arguments to it 
 
   const counter = useSelector(state => state.counter);// Accepts function and executed bu redux-react
   
+  const increamentHandler = () => {
+    // when we call this function it will dispatch an action against our react store
+    dispatch({ type: 'increament' })
+  };
+  const decrementHandler = () => {
+    dispatch({ type: 'decreament' })
+  };
   
   const toggleCounterHandler = () => {};
 
@@ -12,6 +20,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter+1}</div>
+      <div className='counter'>
+        <button onClick={increamentHandler}>Increament</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
