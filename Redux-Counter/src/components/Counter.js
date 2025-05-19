fcec9e309b14ce32e775a96efe6cb90003d9ec80
Rajesh <grajesh2906@gmail.com>
2025-05-19 20:10:02 +0530

    using store data in counter

--- Redux-Counter/src/components/Counter.js
+++ Redux-Counter/src/components/Counter.js
@@ -1,12 +1,17 @@
 import classes from './Counter.module.css';
+import { useSelector } from 'react-redux';// useSelector is a hook developed by react-redux
 
 const Counter = () => {
+
+  const counter = useSelector(state => state.counter);// Accepts function and executed bu redux-react
+  
+  
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter+1}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- Redux-Counter/src/index.js
+++ Redux-Counter/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Provider } from 'react-redux';
-import store from './store/index.js';
+import { Provider } from 'react-redux';// Importing Provider rfunction
+import store from './store/index.js';// Importing store data from custom create dstore in store folder
 
 import './index.css';
 import App from './App';
