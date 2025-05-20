4da86c0bfbbf35e5a89a9c7f4d5c830ce762b5ca
Rajesh <grajesh2906@gmail.com>
2025-05-20 22:31:05 +0530

    Adding Notification status component when sende data using UI-slice componet using redux

--- Redux-Advance/src/App.js
+++ Redux-Advance/src/App.js
@@ -2,25 +2,60 @@ import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
 
 function App() {
   const showCart =  useSelector(state => state.ui.cartisVisible);
-
+  const dispatch = useDispatch();
   //getting cart data
-  const cart = useSelector((state) => state.cart)
+  const cart = useSelector((state) => state.cart);
+
+  const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    fetch('https://react-testing-c78d6-default-rtdb.firebaseio.com/cart.json',{
+    const sendCartData = async () =>{
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'sending cart data !',
+      }))
+      const response = await fetch('https://react-testing-c78d6-default-rtdb.firebaseio.com/cart.json',{
       method: 'PUT',
       body: JSON.stringify(cart),
     });
-  }, [cart]);
+
+    if(!response.ok){
+      throw new Error('Sending cart data failed')
+    }
+    dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success...',
+        message: 'sent cart data successfully !',
+      }))
+    // const responseData = await response.json();
+
+    };
+
+    //calling the function this is async so it retun a promise
+    sendCartData().catch((error) =>{
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error...',
+        message: 'sending cart data failed..!',
+      }));
+    });
+    
+  }, [cart, dispatch]);
   return (
+    <>
     <Layout>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
       {showCart && <Cart /> }
       <Products />
     </Layout>
+    </>
   );
 }
 
--- Redux-Advance/src/store/ui-slice.js
+++ Redux-Advance/src/store/ui-slice.js
@@ -2,10 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
     name: 'ui',
-    initialState: { cartisVisible: false },
+    initialState: { cartisVisible: false, notification:null },
     reducers: {
         toggle(state){
             state.cartisVisible = !state.cartisVisible;
+        },
+        showNotification(state, action){
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message,
+            };
         }
     }
 });
