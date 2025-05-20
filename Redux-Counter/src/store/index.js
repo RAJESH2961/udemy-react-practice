5ec75df893e0e1c522b34c5245930e5a8ae3de27
Rajesh <grajesh2906@gmail.com>
2025-05-20 13:53:11 +0530

    Created slices in redux toolkit

--- Redux-Counter/src/store/index.js
+++ Redux-Counter/src/store/index.js
@@ -1,9 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
 //index.js 
 // Here the showCOunter is added to work eith multiple states
 // it will be false when i click toggle button in Counter.js
 import { createStore } from 'redux';
 
 const initialState = { counter: 0, showCounter: true }
+
+
+// Creating a slice of the Redux state for the "counter" feature
+createSlice({
+    // Unique name for this slice (used in action types like 'counter/increament')
+    name: 'counter',
+
+    // Initial state of this slice
+    initialState,
+
+    // Reducers: Define how the state should change based on actions
+    reducers: {
+        // Increments the counter value by 1
+        increament(state) {
+            // Thanks to Immer (used internally by Redux Toolkit), we can safely mutate state
+            state.counter++;
+        },
+
+        // Decrements the counter value by 1
+        decrement(state) {
+            state.counter--;
+        },
+
+        // Increases the counter by a custom value passed in the action payload
+        increase(state, action) {
+            // action.payload is preferred over action.amount
+            state.counter = state.counter + action.payload;
+        },
+
+        // Toggles the visibility of the counter (true <-> false)
+        toggle(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
 
 const counterReducer = (state = initialState, action) => {
     if(action.type === 'increament') {
