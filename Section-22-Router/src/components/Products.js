12482a301398241f7319913bf93d405e90c48cfe
Rajesh <grajesh2906@gmail.com>
2025-05-21 19:21:04 +0530

    Added Dynamic routes

--- Section-22-Router/src/components/Products.js
+++ Section-22-Router/src/components/Products.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+//Imagine this data is fetching from backend 
+const PRODUCTS = [
+  {id: 'p1', title: 'Product 1'},
+  {id: 'p2', title: 'Product 2'},
+  {id: 'p3', title: 'Product 3'}
+]
 
 const Products = () => {
   return (
     <>
     <h2>The Prodcuts pade</h2>
     <ul>
-      <li>Product -1</li>
-      <li>Product -2</li>
+      {PRODUCTS.map( (prod) => (<li key={prod.id}><Link to={`/products/${prod.id}`}>{prod.title}</Link></li>) )}
+
+      {/* <li><Link to>Product -1</Link></li>
+      <li><Link to>Product -2</Link></li>
+      <li><Link to>Product -3</Link></li>
+      <li><Link>Product -4</Link></li> */}
+      {/* <li>Product -2</li>
       <li>Product -3</li>
       <li>Product -4</li>
-      <li>Product -5</li>
+      <li>Product -5</li> */}
     </ul>
     </>
   )
