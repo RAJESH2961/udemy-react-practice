153c9ce15c71131189609db0c3a5d1f445284699
Rajesh <grajesh2906@gmail.com>
2025-05-21 15:50:05 +0530

    Second method for creating routes

--- Section-22-Router/src/App.js
+++ Section-22-Router/src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Products from './components/Products';
 
-const router = createBrowserRouter([
-  {
-    path: '',
-    element: <HomePage />,
-  },
-  {
-    path: '/products',
-    element: <Products />,
-  },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path='/' element={<HomePage/>} />
+    <Route path='/products' element={<Products/>} />
+  </Route>
+);
+
+//Method -1
+// const router = createBrowserRouter([
+//   {
+//     path: '',
+//     element: <HomePage />,
+//   },
+//   {
+//     path: '/products',
+//     element: <Products />,
+//   },
+// ]);
+
+
+// Method-2
+const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
