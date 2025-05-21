79b3b1da6378543aad31e34d24331f42a54fc5ea
Rajesh <grajesh2906@gmail.com>
2025-05-21 15:45:25 +0530

    added second  route

--- Section-22-Router/src/App.js
+++ Section-22-Router/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import Products from './components/Products';
 
 const router = createBrowserRouter([
   {
     path: '',
     element: <HomePage />,
+  },
+  {
+    path: '/products',
+    element: <Products />,
   },
 ]);
 
