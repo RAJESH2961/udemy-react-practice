75258fcc215394ae57f6cc1afc6063f86a8ab42c
Rajesh <grajesh2906@gmail.com>
2025-05-22 18:27:17 +0530

    Added json utility() for throwing an error

--- Section-22-Router-1/frontend/src/pages/Events.js
+++ Section-22-Router-1/frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom';
+import { json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
@@ -28,7 +28,12 @@ export async function loader() {
     //   return {isError : true, message: 'Could not fetch events'}
 
     //this thrown error is captured by useRouteError() which we are calling in Error.js
-    throw new Response(JSON.stringify({ message: 'Could not fetch events' }), {status: 500,});
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events' }), {status: 500,});
+
+    throw json(
+        { message: 'Could not fetch events' },
+        {status: 500,}
+    )
     }
 
     const resData = await response.json();
