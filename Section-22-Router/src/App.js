c71a446ed335811d24abaa88586d3e82175971df
Rajesh <grajesh2906@gmail.com>
2025-05-22 10:57:47 +0530

    Index route

--- Section-22-Router/src/App.js
+++ Section-22-Router/src/App.js
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,// Error page when url enters wrong
     children: [
       {
-        path: '/',
+        index: true,
         element: <HomePage />,
       },
       {
