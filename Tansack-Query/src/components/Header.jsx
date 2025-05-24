c1e6def3c2100fe17e6e5d82d530f0a8120fd6d5
Rajesh <grajesh2906@gmail.com>
2025-05-24 23:32:43 +0530

    reactquery with react routre and adding global progress bar

--- Tansack-Query/src/components/Header.jsx
+++ Tansack-Query/src/components/Header.jsx
@@ -1,7 +1,17 @@
+import { useIsFetching } from "@tanstack/react-query";
+
+useIsFetching
 export default function Header({ children }) {
+  // fetching is an number 0 if not fetchind data is not fetching anyehre in the applicaiton
+  const fetching = useIsFetching();
   return (
     <>
-      <div id="main-header-loading"></div>
+      <div id="main-header-loading">
+        {/* // default elemtn in html */}
+         {/* showing progress bar when fetching data globally int he header  */}
+         {fetching > 0 && <progress /> }
+        
+      </div>
       <header id="main-header">
         <div id="header-title">
           <h1>React Events</h1>
