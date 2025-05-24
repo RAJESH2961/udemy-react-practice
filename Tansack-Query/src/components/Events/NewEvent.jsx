a6fc821ab33139adb4dac847f2cd40616e944e1f
Rajesh <grajesh2906@gmail.com>
2025-05-24 18:03:29 +0530

    refresing event without going to another page using invalidateQueries

--- Tansack-Query/src/components/Events/NewEvent.jsx
+++ Tansack-Query/src/components/Events/NewEvent.jsx
@@ -5,16 +5,25 @@ import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { createNewEvent } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
-
+import { queryClient } from '../../util/http.js';
 export default function NewEvent() {
   const navigate = useNavigate();
 
   const { mutate, isPending, isError, error } = useMutation({
-    mutationFn: createNewEvent
+    mutationFn: createNewEvent,
+    // on success ensures the fucntion will be executed only if the mutation is success
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['events']}); // invalidate queries refreshes the data it tells to reach that the data is outdated and refreshes
+      navigate('/events');
+    }
   })
 
   function handleSubmit(formData) {
     mutate({ event: formData });
+    // no matter if the mutataion success or not it always redirect to navigate page
+    // navigate('/events'); // it will redirect to events if didnt entered valid data also
+    // we need to wait until mutataion gets completed
+    // to solve this useMutataion provides onSuccess()
   }
 
   return (
