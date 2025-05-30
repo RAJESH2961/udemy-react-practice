677ab95603260f7134c5592abf4e583e9f20ebb4
Rajesh <grajesh2906@gmail.com>
2025-05-30 10:48:14 +0530

    Added functions and function Types

--- Type-Script/basic.ts
+++ Type-Script/basic.ts
@@ -35,7 +35,38 @@ peoples = [
 
 // let course: string = 'React- The Complete Guide' // Explicitily Declaring Types
 // Type Inference is a feature in languages like TypeScript where the compiler automatically deduces (infers) the type of a variable based on the value assigned to it, without you needing to explicitly annotate it.
-let course = 'React- The Complete Guide';
+// let course = 'React- The Complete Guide';
 let sName = "Alice";       // inferred as string
 let isStudent = true;     // inferred as boolean
 let scores = [90, 80, 70]; // inferred as number[]
+
+
+let course : string | number[] = "some data"
+
+// Allias Examples
+type Person = {
+    name: string;
+    age: number;
+};
+
+let p1: Person = {
+    name: "Rajesh",
+    age: 20
+};
+let p2: Person = {
+    name: "Rajesh",
+    age: 20
+};
+
+// Functions & types
+function add(a: number, b: number) {
+    return a+b;
+}
+function add(a: number, b: number): number {
+    return a+b;
+}
+
+function print(value: any): void{
+    console.log(value);
+    
+}
