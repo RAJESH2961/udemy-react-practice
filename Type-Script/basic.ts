342d8d3e3c9726835d70d376b9060867c38a9d26
Rajesh <grajesh2906@gmail.com>
2025-05-30 08:34:40 +0530

    Practice base types

---
+++ Type-Script/basic.ts
@@ -0,0 +1,32 @@
+// Primitives
+let age: number;
+age = 20;
+
+// Complex data types
+
+let person: {
+    name: string;
+    age: number;
+}
+// assignining values
+person = {
+    name: 'Rajesh',
+    age: 20
+}
+
+// Array of peoples
+let peoples: {
+    name: string;
+    age: number;
+}[];
+// array of objects
+peoples = [
+    {
+        name: 'Rajesh',
+        age: 20
+    },
+    {
+        name: 'Rajesh',
+        age: 20
+    }
+];
